f5be24b0861bbfdb69e3af984c5aed852b6dd862
NicolasMCompSci <115744859+NicolasMCompSci@users.noreply.github.com>
2024-11-18 20:48:26 -0500

    quick push

--- NicolasMasquieto_DesignSprint/Source.cpp
+++ NicolasMasquieto_DesignSprint/Source.cpp
@@ -13,15 +13,18 @@ using namespace std;
 // constant strings
 const string InputFileName = "Hamlet.txt";
 const string horatioWord = "horatio";
-const string horatioWordPossessive = "horatios"; // accounts for possessive uses
+const string horatioWordPossessive = "horatios";
 const string andWord = "and";
 const string hamletWord = "hamlet";
-const string hamletWordPossessive = "hamlets"; // accounts for possessive uses
+const string hamletWordPossessive = "hamlets";
 const string godWord = "god";
-const string godWordPossessive = "gods"; // accounts for possessive uses
+const string godWordPossessive = "gods";
 
-// Mutex for updating shared counters
-mutex sharedCountsMutex;
+mutex allWordCountSem;
+mutex horatioCountSem;
+mutex andCountSem;
+mutex hamletCountSem;
+mutex godCountSem;
 
 // cleans word of any symbols such as commas
 string CleanWord(const string& word) {
@@ -40,8 +43,35 @@ string ToLower(const string& word) {
     return lower;
 }
 
+class Semaphore {
+public:
+    Semaphore(unsigned long init_count) {
+        count = init_count;
+    }
+
+    void acquire() { // decrement the internal counter
+        std::unique_lock<std::mutex> Lock(myMutex);
+        while (!count) {
+            Condition.wait(Lock);
+        }
+        count--;
+    }
+
+    void release() { // increment the internal counter
+        std::unique_lock<std::mutex> Lock(myMutex);
+        count++;
+        Lock.unlock();
+        Condition.notify_one();
+    }
+
+private:
+    std::mutex myMutex;
+    std::condition_variable Condition;
+    unsigned long count;
+};
+
 // looks for elements to count in a line and increments them
-void CountThroughLine(const string& line, int& horatioCount, int& andCount, int& hamletCount, int& godCount, int& allWordCount) {
+void CountThroughLine(string& line, int& horatioCount, int& andCount, int& hamletCount, int& godCount, int& allWordCount) {
 
     stringstream stream(line);
     string word;
@@ -54,17 +84,31 @@ void CountThroughLine(const string& line, int& horatioCount, int& andCount, int&
         // if word isn't empty check if it is one of the words we're looking for
         if (!cleanedWord.empty()) {
             // check if word is one we're looking for
-            if (cleanedWord == horatioWord || cleanedWord == horatioWordPossessive)
+            if (cleanedWord == horatioWord || cleanedWord == horatioWordPossessive) {
+                horatioCountSem.lock();
                 horatioCount++;
-            else if (cleanedWord == andWord)
+                horatioCountSem.unlock();
+            }
+            else if (cleanedWord == andWord) {
+                andCountSem.lock();
                 ++andCount;
-            else if (cleanedWord == hamletWord || cleanedWord == hamletWordPossessive)
+                andCountSem.unlock();
+            }
+            else if (cleanedWord == hamletWord || cleanedWord == hamletWordPossessive) {
+                hamletCountSem.lock();
                 hamletCount++;
-            else if (cleanedWord == godWord || cleanedWord == godWordPossessive)
+                hamletCountSem.unlock();
+            }
+            else if (cleanedWord == godWord || cleanedWord == godWordPossessive) {
+                godCountSem.lock();
                 godCount++;
+                godCountSem.unlock();
+            }
 
             // increase word count
+            allWordCountSem.lock();
             allWordCount++;
+            allWordCountSem.unlock();
         }
     }
 }
@@ -72,53 +116,45 @@ void CountThroughLine(const string& line, int& horatioCount, int& andCount, int&
 int main() {
 
     // open file
-	ifstream inputFile(InputFileName);
+    ifstream inputFile(InputFileName);
     // check if file opened successfully
-	if (!inputFile) {
-		cerr << "could not open " << InputFileName << endl;
-		return 1;
-	}
+    if (!inputFile) {
+        cerr << "could not open " << InputFileName << endl;
+        return 1;
+    }
 
     // establish words that will be counted
-    int totalWordCount = 0;
-    int totalHoratioCount = 0;
-    int totalAndCount = 0;
-    int totalHamletCount = 0;
-    int totalGodCount = 0;
+    int allWordCount = 0;
+    int horatioCount = 0;
+    int andCount = 0;
+    int hamletCount = 0;
+    int godCount = 0;
+
+    // set semaphores for all variables
+    //Semaphore allWordCountSem(1);
+    //Semaphore horatioCountSem(1);
+    //Semaphore andCountSem(1);
+    //Semaphore hamletCountSem(1);
+    //Semaphore godCountSem(1);
 
     // set the number of threads for boost pool to use
     boost::asio::thread_pool pool(thread::hardware_concurrency());
 
     // read through every line in parallel
-	string line;
-    while (getline(inputFile, line)) {
-        boost::asio::post(pool, [line, &totalHoratioCount, &totalAndCount, &totalHamletCount, &totalGodCount, &totalWordCount]() {
-            // Local counters
-            int horatioCount = 0, andCount = 0, hamletCount = 0, godCount = 0, allWordCount = 0;
-
-            CountThroughLine(line, horatioCount, andCount, hamletCount, godCount, allWordCount);
-
-            // Update shared counters
-            lock_guard<mutex> lock(sharedCountsMutex);
-            totalHoratioCount += horatioCount;
-            totalAndCount += andCount;
-            totalHamletCount += hamletCount;
-            totalGodCount += godCount;
-            totalWordCount += allWordCount;
-            });
-    }
-        //boost::asio::post(pool, [&]() { CountThroughLine(line, horatioCount, andCount, hamletCount, godCount, allWordCount); });
+    string line;
+    while (getline(inputFile, line))
+        boost::asio::post(pool, [&]() { CountThroughLine(line, horatioCount, andCount, hamletCount, godCount, allWordCount); });
 
     // join pool and close file
     pool.join();
     inputFile.close();
 
     // output results
-    cout << totalHoratioCount << " " << horatioWord << endl;
-    cout << totalAndCount << " " << andWord << endl;
-    cout << totalHamletCount << " " << hamletWord << endl;
-    cout << totalGodCount << " " << godWord << endl;
-    cout << "Total word count = " << totalWordCount << endl;
+    cout << horatioCount << " " << horatioWord << endl;
+    cout << andCount << " " << andWord << endl;
+    cout << hamletCount << " " << hamletWord << endl;
+    cout << godCount << " " << godWord << endl;
+    cout << "Total word count = " << allWordCount << endl;
 
     return 0;
 }
